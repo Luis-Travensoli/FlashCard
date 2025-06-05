9787ee9bf225be442de2eae6d4a8ec245ea59fa3
Luis-Travensoli <travensoli.luis@escola.pr.gov.br>
2025-06-05 08:28:26 -0300

    fpipi

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'História',
+    'Em que ano começou a Segunda Guerra Mundial?',
+    'Em 1939'
+)
+
+criaCartao(
+    'Medidas de terreno',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
